a4ced00bbb40370369664786c57570ec6ec98966
Karly Mareka <karly.mareka@gmail.com>
2019-03-27 01:48:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,29 +7,29 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, SUM(pledges.amount)
-FROM pledges
-INNER JOIN projects
-ON pledges.project_id = projects.id
-GROUP BY projects.title 
-ORDER BY projects.title ASC;"
+  "SELECT projects.title, SUM(pledges.amount)
+  FROM pledges
+  INNER JOIN projects
+  ON pledges.project_id = projects.id
+  GROUP BY projects.title 
+  ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, SUM(pledges.amount)
-FROM pledges
-INNER JOIN users
-ON pledges.user_id = users.id
-GROUP BY users.name
-ORDER BY users.name ASC"
+  "SELECT users.name, users.age, SUM(pledges.amount)
+  FROM pledges
+  INNER JOIN users
+  ON pledges.user_id = users.id
+  GROUP BY users.name
+  ORDER BY users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal)
-FROM pledges
-INNER JOIN projects
-ON pledges.project_id = projects.id
-GROUP BY projects.title HAVING (SUM(pledges.amount) - projects.funding_goal) >= 0";
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal)
+  FROM pledges
+  INNER JOIN projects
+  ON pledges.project_id = projects.id
+  GROUP BY projects.title HAVING (SUM(pledges.amount) - projects.funding_goal) >= 0";
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
